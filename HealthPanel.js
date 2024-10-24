5cc05bdd4af2d7f8965b3681233b5be6f5160f2a
Eva0894 <149769045+Eva0894@users.noreply.github.com>
2024-10-25 04:50:28 +1100

    Update HealthPanel.js

--- HealthPanel.js
+++ HealthPanel.js
@@ -1,14 +1,1 @@
-import React from 'react';
 
-function HealthPanel() {
-  return (
-    <div className="health-panel">
-      <h3>Health Monitoring</h3>
-      <p>Heart Rate: 72 bpm</p>
-      <p>Steps Today: 3000</p>
-      <button>Update Health Status</button>
-    </div>
-  );
-}
-
-export default HealthPanel;
