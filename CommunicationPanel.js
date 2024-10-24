9eb63e64ba96714b15b115f6894da2889ab07c44
Eva0894 <149769045+Eva0894@users.noreply.github.com>
2024-10-25 04:49:53 +1100

    Update CommunicationPanel.js

--- CommunicationPanel.js
+++ CommunicationPanel.js
@@ -1,13 +1,1 @@
-import React from 'react';
 
-function CommunicationPanel() {
-  return (
-    <div className="communication-panel">
-      <h3>Talk to CareMate</h3>
-      <textarea placeholder="Type your message here..." />
-      <button>Send</button>
-    </div>
-  );
-}
-
-export default CommunicationPanel;
