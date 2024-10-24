216f1da6902ca3de5b603eff02ea55fc0dc24f13
Eva0894 <149769045+Eva0894@users.noreply.github.com>
2024-10-25 04:49:43 +1100

    Update EmergencyButton.js

--- EmergencyButton.js
+++ EmergencyButton.js
@@ -1,11 +1,1 @@
-import React from 'react';
 
-function EmergencyButton() {
-  return (
-    <div className="emergency-button">
-      <button>Emergency Call</button>
-    </div>
-  );
-}
-
-export default EmergencyButton;
